930b9a4b580c573b5956b0d905fd5772f015aeb3
9kuldeep <kuldeepsingh9759397@gmail.com>
2023-08-09 13:38:52 +0530

    blockchain pro

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+// export const contractAddress = "0xfd28C340E31D2C3821B4cF12f5eCB601e125bf40";
+export const contractAddress = "0x96236a80ac968d216d3669fd467435D404Ca50Da";
+export const contractABI = abi.abi;
